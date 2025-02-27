3dd9e614e674bc3dbce25412d39dadf2a79db6f3
Mehdi Hosseinzadeh <mehdi.hosseinzadeh@adelaide.edu.au>
2025-02-28 00:44:30 +1030

    minor change in config

--- slurm_train.sh
+++ slurm_train.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #SBATCH -p a100 -A strategic               # Partition/queue with A100 GPUs
-#SBATCH --job-name=noposplat     # Optional: name of your job
 #SBATCH --nodes=6             # 6 nodes
 #SBATCH --ntasks-per-node=4   # 4 tasks per node (total 24 tasks for 6 nodes)
 #SBATCH --gres=gpu:4          # 4 GPUs per node (A100)
@@ -34,4 +33,4 @@ echo "Starting job: $SLURM_JOB_NAME with ID $SLURM_JOB_ID"
 echo "Using TMPDIR: $TMPDIR"
 echo "Nodes allocated: $SLURM_NODELIST"
 
-srun --export=ALL python -m src.main +experiment=re10k wandb.mode=offline
+srun --export=ALL python -m src.main +experiment=re10k_normal wandb.mode=offline
