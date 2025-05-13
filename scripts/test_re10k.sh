fdd8148df6f648983ee405686f0dcc3d9af95ec7
Mehdi Hosseinzadeh <mehdi.hosseinzadeh@adelaide.edu.au>
2025-05-13 15:05:52 +0930

    added/modified configs and scripts

--- scripts/test_re10k.sh
+++ scripts/test_re10k.sh
@@ -1,9 +1,9 @@
 # In-Domain NVS Evaluation on RE10k
 CUDA_VISIBLE_DEVICES=0 python -m src.main \
-                        +experiment=re10k_grid_1x8 \
+                        +experiment=re10k_grid_normal \
                         mode=test \
-                        wandb.name=test_re10k_grid \
+                        wandb.name=test_re10k_grid_normal_v2 \
                         dataset/view_sampler@dataset.re10k.view_sampler=evaluation \
                         dataset.re10k.view_sampler.index_path=assets/evaluation_index_re10k.json \
                         test.save_image=true \
-                        checkpointing.load=./pretrained_weights/ours_re10k_grid_hpc_2025-03-01_22-56-21_step_18748.ckpt
+                        checkpointing.load=./pretrained_weights/ours_re10k_grid_normal_v2_gaussians-detached-grid_hpc_2025-03-15_20-47-46_step_18748.ckpt 
---
+++ slurm_eval.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#SBATCH -p a100 -A strategic               # Partition/queue with A100 GPUs
+#SBATCH --nodes=1             # 6 nodes
+#SBATCH --ntasks-per-node=1   # 4 tasks per node (total 24 tasks for 6 nodes)
+#SBATCH --gres=gpu:1          # 4 GPUs per node (A100)
+#SBATCH --cpus-per-task=4     # Number of CPU cores per GPU process
+#SBATCH --time=24:00:00       # Time limit (D-HH:MM)
+#SBATCH --mem=64GB            # Memory per node
+
+# Notification configuration 
+#SBATCH --mail-type=END       # Send a notification email when the job is done
+#SBATCH --mail-type=FAIL      # Send a notification email when the job fails
+#SBATCH --mail-user=mehdi.hosseinzadeh@adelaide.edu.au  # Email to receive notifications
+
+source ~/.bashrc
+module load CUDA/11.8.0
+module load GCC/11.2.0
+module use /apps/icl/modules/all
+
+source ~/.bashrc
+conda activate /hpcfs/users/a1733633/project/Gen2DGS/.conda
+
+Redirect temporary files to a directory with sufficient space
+export TMPDIR=/hpcfs/users/a1733633/tmp
+export TMP=/hpcfs/users/a1733633/tmp
+export TEMP=/hpcfs/users/a1733633/tmp
+export WANDB_CACHE_DIR=/hpcfs/users/a1733633/tmp/wandb_cache
+mkdir -p $TMPDIR
+mkdir -p $WANDB_CACHE_DIR
+
+echo "Starting job: $SLURM_JOB_NAME with ID $SLURM_JOB_ID"
+echo "Using TMPDIR: $TMPDIR"
+echo "Nodes allocated: $SLURM_NODELIST"
+
+srun --export=ALL python -m src.main \
+                            +experiment=re10k_grid_normal \
+                            mode=test \
+                            wandb.name=test_re10k_grid_normal_v2 \
+                            dataset/view_sampler@dataset.re10k.view_sampler=evaluation \
+                            dataset.re10k.view_sampler.index_path=assets/evaluation_index_re10k.json \
+                            test.save_image=false \
+                            checkpointing.load=./pretrained_weights/ours_re10k_grid_normal_v2_gaussians-detached-grid_hpc_2025-03-15_20-47-46_step_18748.ckpt \
+                            > "nvs_re10k_grid_normal_with-pose-refinement_ours_re10k_grid_normal_v2_gaussians-detached-grid_hpc_2025-03-15_20-47-46_step_18748.txt" 2>&1
+    
